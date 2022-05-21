44d12acd5afb17d01ff14eb48549ddaf6e574211
TsmRobin <r.l.moret@student.rug.nl>
2022-05-21 20:33:49 +0200

    negamax with transposition table

--- nim.py
+++ nim.py
@@ -29,12 +29,58 @@ def min_value(state):
     return min
 
 
-def minimax_decision(state, turn):
+def negamax(state, turn, max_tt, min_tt):
+    best_move = None
+    value = 0
+
+    if state == 1:
+        if turn == 0:
+            return -1, 0
+        else:
+            return 1, 0
+
+    if turn == 0:  # MAX' turn
+        max = -100000000000
+
+        # for all moves, make the one with the best guaranteed value
+        for move in range(1, 4):
+            if state - move > 0:
+                if min_tt.get(state-move) is not None:
+                    m = min_tt[state-move]
+                else:
+                    m, min_move = negamax(state - move, 1, max_tt, min_tt)
+                    min_tt[state - move] = m
+                if m > max:
+                    max = m
+                    best_move = move
+                    value = max
+
+    else:
+        min = 10000000000000
+
+        # pick move with worst guaranteed value
+        for move in range(1, 4):
+            if state - move > 0:
+                if max_tt.get(state-move) is not None:
+                    m = max_tt[state - move]
+                else:
+                    m, max_move = negamax(state-move, 0, max_tt, min_tt)
+                    max_tt[state-move] = m
+                if m < min:
+                    min = m
+                    best_move = move
+                    value = min
+
+    return value, best_move
+
+
+def minimax(state, turn, max_tt, min_tt):
     best_move = None
 
     if turn == 0:  # MAX' turn
         max = -100000000000
 
+        # for all moves, make the one with the best guaranteed value
         for move in range(1, 4):
             if state - move > 0:
                 m = min_value(state - move)
@@ -45,6 +91,7 @@ def minimax_decision(state, turn):
     else:
         min = 10000000000000
 
+        # pick move with worst guaranteed value
         for move in range(1, 4):
             if state - move > 0:
                 m = max_value(state-move)
@@ -52,14 +99,16 @@ def minimax_decision(state, turn):
                     min = m
                     best_move = move
 
-    return best_move
+    return 0, best_move
 
 
 def play_nim(state):
     turn = 0
+    max_tt = {}
+    min_tt = {}
 
     while state != 1:
-        move = minimax_decision(state, turn)
+        value, move = negamax(state, turn, max_tt, min_tt)
         print(str(state) + ": " + ("MAX" if not turn else "MIN") + " takes " + str(move))
 
         state -= move
